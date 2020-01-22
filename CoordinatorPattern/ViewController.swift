d89c942bb3afd6267913ef70e91291f7301f0ecf
mohamedzead1996 <fci.scu.mohmadmahmoudzead@gmail.com>
2020-01-22 17:00:26 +0200

    Initial Commit

---
+++ CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Mac on 1/22/20.
+//  Copyright © 2020 spark-cloud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
